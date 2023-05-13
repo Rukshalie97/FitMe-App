04e0c13acdc2cd28d34fda08049df295e9da033b
Rukshalie97 <trukshalieinfo@gmail.com>
2023-05-13 22:41:22 +0530

    init xcode

---
+++ Fit Me/Fit Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fit Me
+//
+//  Created by Rukshalie  on 2023-05-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
